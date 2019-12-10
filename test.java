2f94cfe9d784a6daed758b93afe13b769d9d0722
angel <angel@163.com>
2019-12-10 20:06:31 +0800

    angle新建分支

--- test.java
+++ test.java
@@ -8,4 +8,8 @@ public class test{
 	public static void test(){
 		System.out.println("我欲乘风归去，又恐琼楼玉宇，高处不胜寒起舞弄清影，何似在人间");
 	}
+	public static void test(){
+		System.out.println("转朱阁，低绮户，照无眠，不应有恨");
+	}
+	
 }
