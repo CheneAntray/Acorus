458a452dedea74ac06776f4004ee04bfeaafe1ac
angel <angel@163.com>
2019-12-13 20:10:21 +0800

    angel的版本与acorus的回滚版本冲突

--- test.java
+++ test.java
@@ -6,7 +6,7 @@ public class test{
 	}
 
 	public static void test(){
-		System.out.println("迪迦奥特曼，浓睡不消残酒，试问卷帘人，却道海棠依旧，知否，知否，应是绿肥红瘦");
+		System.out.println("女词人李清照，浓睡不消残酒，试问卷帘人，却道海棠依旧，知否，知否，应是绿肥红瘦");
 	}
 	public static void test(){
 		System.out.println("转朱阁，低绮户，照无眠，不应有恨");
